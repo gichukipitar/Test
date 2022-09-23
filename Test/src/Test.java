5cdfc042d932765858543af099ee5598888d0759
SIRH PITAR <SIRH PITAR@DESKTOP-MS17DVB.mshome.net>
2022-09-24 01:17:37 +0300

    Hope you find this well and good

---
+++ Test/src/Test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner test = new Scanner(System.in);
+		System.out.println("What is your name");
+		test.nextLine();
+		test.close();
+
+	}
+
+}
